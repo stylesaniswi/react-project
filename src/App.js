0099ae2b4771af973a3df27462bc83213d3669e1
stylesaniswi <anishshrestha607@gmail.com>
2022-06-02 17:09:52 +0545

    my-app commited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Todolist from "./Todolist";
 
 function App() {
+  const [inputList, setInputList]= useState("");
+  const [items, setItems] = useState([]);
+
+  const itemEvent=(event)=>{
+    setInputList(event.target.value);
+  };
+  const listOfItems=(e)=>{
+    if(!inputList){
+      alert("enter the task!!");
+      return;
+    }
+    setItems((oldItems)=>{
+    
+      return [...oldItems, inputList];
+      
+    })
+    setInputList("");
+  };
+
+  const deleteItems=(id)=>{
+    setItems((oldItems)=>{
+      return oldItems.filter((arrElem, index)=>{
+        return index !==id;
+      });
+    });
+   
+};
+
+const doneItems=(id)=>{
+  document.getElementsByTagName('li')[id].classList.add('checked');
+}
+
+const apiCall=()=>{
+  fetch("https://www.boredapi.com/api/activity")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setItems((oldItems)=>{
+            return [...oldItems, result.type]
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          alert(error);
+        }
+      )
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="container">
+        <br />
+        <h2> Todays task!!</h2>
+        <br />
+        <input type='text' placeholder='write the task' value={inputList} onChange={itemEvent} />
+      <button onClick={listOfItems}>add</button>
+      <button onClick={apiCall}>API call</button>
+      <ul>
+        {items.map((itemval, index)=>{
+          return <Todolist
+          text={itemval}
+          key={index}
+          id={index}
+          onSelect={deleteItems}
+          onChecked={doneItems}
+           />
+        })}
+      </ul>
+      </div>
     </div>
   );
 }
